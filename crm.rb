149987cc69c6d18091289b729c7f82b6557a0b92
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-15 17:40:20 -0500

    Added home, about, all contacts, etc. functionality

--- crm.rb
+++ crm.rb
@@ -5,9 +5,21 @@ get '/' do
   erb :index
 end
 
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/' do
+  redirect :contacts
+end
+
 get '/contacts/:id' do
-  @contact = Contact.find(params[:id])
-  erb :show_contact
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get '/about' do
